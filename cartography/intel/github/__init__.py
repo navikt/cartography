7720dbcd6fd4bf1fc5b175dd42d82672bc0fee4e
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2026-07-28 08:38:11 +0200

    bring back _write_synced_pushedat import

--- cartography/intel/github/__init__.py
+++ cartography/intel/github/__init__.py
@@ -22,6 +22,7 @@ import cartography.intel.github.users
 from cartography.client.core.tx import read_list_of_values_tx
 from cartography.config import Config
 from cartography.intel.github.app_auth import make_credential
+from cartography.intel.github.repos import _write_synced_pushedat
 from cartography.util import timeit
 
 logger = logging.getLogger(__name__)
